d0d85d175a6317f77e328feaf140c23cccb0fea1
atteluot <atteluot@metropolia.fi>
2026-08-20 13:15:24 +0300

    lisäsin vikan tehtävän module 2

---
+++ Moduuli 2/teht6.py
@@ -0,0 +1,12 @@
+import random
+
+noppa1_1 = random.randint(0,9)
+noppa1_2 = random.randint(0,9)
+noppa1_3 = random.randint(0,9)
+print(noppa1_1,noppa1_2,noppa1_3)
+
+noppa2_1 = random.randint(1,6)
+noppa2_2 = random.randint(1,6)
+noppa2_3 = random.randint(1,6)
+noppa2_4 = random.randint(1,6)
+print(noppa2_1,noppa2_2,noppa2_3,noppa2_4)
