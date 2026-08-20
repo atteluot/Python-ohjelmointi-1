58515f36dcf561cf1db9613aea85bb7bc1b0bf21
atteluot <atteluot@metropolia.fi>
2026-08-20 10:33:04 +0300

    lisäsin 2moduuliin tehtävän

---
+++ Moduuli 2/teht4.py
@@ -0,0 +1,11 @@
+numero1 = int(input("Anna ensimmäinen kokonais luku:"))
+numero2 = int(input("Anna toinen kokonais luku:"))
+numero3 = int(input("Anna kolmas kokonais luku:"))
+
+summa = numero1 + numero2 + numero3
+tulo = numero1 * numero3 * numero2
+keskiarvo = summa / 3
+
+print(summa)
+print(tulo)
+print(keskiarvo)
