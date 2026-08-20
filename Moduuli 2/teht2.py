9b6d486af881d6ea14b908f8db054064749e8b20
atteluot <atteluot@metropolia.fi>
2026-08-20 10:00:48 +0300

    Lisäsin mooduli 2 koska aloitin sen ja tuntitehtävän

---
+++ Moduuli 2/teht2.py
@@ -0,0 +1,7 @@
+import math
+
+säde = float(input("Anna ympyrän säde:"))
+print(säde)
+
+pintaala = (säde ** 2) * math.pi
+print(pintaala)
---
+++ Moduuli 2/teht3.py
@@ -0,0 +1,6 @@
+kanta = float(input("Anna suorakulmion kanta:"))
+Korkeus = float(input("Anna suorakulmion korkeus:"))
+pintaala = kanta*Korkeus
+piiri = (2 * kanta) + (2 * Korkeus)
+print("Pintaala on:", pintaala)
+print("Piiri on:", piiri)
